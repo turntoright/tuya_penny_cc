01d226037b0b8aed697a6744f868ee347a9ed6ff
harveycao-lincoln <Hongwu.Cao@lincolnuni.ac.nz>
2026-04-15 14:41:04 +1200

    feat(dbt): add fct_energy_intervals incremental fact table

---
+++ dbt/models/marts/fct_energy_intervals.sql
@@ -0,0 +1,31 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['device_id', 'event_ts'],
+        on_schema_change='fail',
+    )
+}}
+
+WITH source AS (
+    SELECT
+        s.device_id,
+        d.name,
+        d.category,
+        s.event_ts,
+        s.prev_event_ts,
+        TIMESTAMP_DIFF(s.event_ts, s.prev_event_ts, MINUTE) AS interval_minutes,
+        s.interval_kwh,
+        s.dp_value / 100.0                                   AS cumulative_kwh,
+        s.interval_kwh < 0                                   AS is_counter_reset
+    FROM {{ ref('stg_energy_dp_log') }} s
+    LEFT JOIN {{ ref('dim_devices') }} d USING (device_id)
+    WHERE s.dp_code = 'add_ele'
+      AND s.interval_kwh IS NOT NULL
+)
+
+SELECT *
+FROM source
+
+{% if is_incremental() %}
+WHERE event_ts > (SELECT MAX(event_ts) FROM {{ this }})
+{% endif %}
